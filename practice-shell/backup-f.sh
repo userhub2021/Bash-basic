dcf7301e63296db42b77c2bbe1510db62c7d7b75
userhub2021 <takahiro12071@outlook.jp>
2026-02-27 14:29:48 +0900

    service_check.shを追加

--- practice-shell/backup-f.sh
+++ practice-shell/backup-f.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-for result in $(ls -lh |grep 50K ); do
-    if [ -eq]
+if [ -d /backup/test1 ]; then
+    echo "Directory exists"
+else
+    mkdir -p /backup/test1
+    echo "backup directory created"
+fi
+
+find /etc -type f -size -10k -name "*.conf" -print0 | while IFS= read -r -d '' result; do
+    cp "$result" /backup/test1
+    echo "File copied: $result"
+done
+
+ls -1 /backup/test1 | wc -l
+exit 0
